38f46e91cf4b61b140470542f109ad9c9e34993b
ejffry21 <ejffry21@gmail.com>
2024-11-17 02:48:59 +0000

    programming challenge 1

---
+++ Activity/ScoreGrade.java
@@ -0,0 +1,18 @@
+public class ScoreGrade {
+    public static void main(String[] args) {
+        int grade = 79;
+        if (grade >=90){
+            System.out.println("Your Grade is: A");   
+        }else if (grade>=80 && grade <=89) {
+            System.out.println("Your Grade is: B");    
+        }else if (grade>=70 && grade <=79) {
+            System.out.println("Your Grade is: C");    
+        }else if (grade>=60 && grade <=69) {
+            System.out.println("Your Grade is: D");    
+        }else if (grade<60) {
+            System.out.println("Your Grade is: F");    
+        }else{
+            System.out.println("invalid input!");
+        }
+    }
+}
